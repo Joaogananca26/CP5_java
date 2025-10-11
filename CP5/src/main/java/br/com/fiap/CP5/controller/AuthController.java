32da91ff90cc2878a189ea642cf99e614f412b98
João Ganança <93109162+Joaogananca26@users.noreply.github.com>
2025-10-11 18:25:53 -0300

    Fazendo a página para login no spring security

---
+++ CP5/src/main/java/br/com/fiap/CP5/controller/AuthController.java
@@ -0,0 +1,13 @@
+package br.com.fiap.CP5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
